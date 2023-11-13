a78b895a4727a1b098fbb968090f337fd2a3f735
Joseph Garcia <josep@ZhayNaz.com>
2023-11-13 09:33:17 +0800

    daily commit

--- game_Functions/gameFunction.js
+++ game_Functions/gameFunction.js
@@ -1,1 +1,36 @@
-console.log("meronq");
+// Get the button element
+var openDialogBtn = document.getElementById('openDialogBtn');
+
+// Add a click event listener to the button
+openDialogBtn.addEventListener('click', function() {
+  // Open a dialog
+  console.log("meron");
+  showDialog();
+});
+
+// Function to open the dialog
+function showDialog() {
+  // Create a new dialog element
+  var dialog = document.createElement('dialog');
+
+  // Set the content of the dialog
+  dialog.innerHTML = `
+    <p>This is a dialog!</p>
+    <button id="closeDialogBtn">Close</button>
+  `;
+
+  // Append the dialog to the document body
+  document.body.appendChild(dialog);
+
+  // Get the close button inside the dialog
+  var closeDialogBtn = dialog.querySelector('#closeDialogBtn');
+
+  // Add a click event listener to the close button
+  closeDialogBtn.addEventListener('click', function() {
+    // Close the dialog
+    dialog.close();
+  });
+
+  // Open the dialog
+  dialog.showModal();
+}
